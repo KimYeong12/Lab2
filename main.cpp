8fab6a038455d978b7ca1dc4b34a1ad1829a6e4c
kim <young6404@snu.ac.kr>
2024-06-07 10:45:40 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -3,20 +3,46 @@
 
 using namespace std;
 
-int N,M;
-vector<int> vec;
-
-void func(int cnt) {
 
 
+void dfs(vector<int>& selec, int N, int M, int index)
+{
+	if (selec.size() == M)
+	{
+		for (int num:selec)
+		{
+			cout << num << " ";
+		}
+		cout << "\n";
+		return;
+	}
 
-
-
-
-
+	for (int i = index;i<=N;i++)
+	{
+		selec.push_back(i);
+		dfs(selec, N, M, i+1);
+		selec.pop_back();
+	}
 }
 
-int main() {
-   cin >> N >> M;
-   func(0);
+
+
+
+
+
+int main()
+{
+
+	int N, M;
+	cin >> N >> M;
+	vector<int> selec;
+	dfs(selec, N, M, 1);
+	
+	return 0;
 }
+
+
+
+
+
+
